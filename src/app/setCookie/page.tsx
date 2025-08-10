6425339edc24a9c4112a68b376949d1e09511693
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2025-08-10 16:21:45 +0900

    remove .tsx

--- src/app/setCookie/page.tsx
+++ src/app/setCookie/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import dynamic from 'next/dynamic';
-import { AppMode } from '../appmode.js';
 
-const DynamicHome = dynamic(() => import('./home.tsx'), { ssr: false });
+const DynamicHome = dynamic(() => import('./home'), { ssr: false });
 
 export default function Home() {
   return (
